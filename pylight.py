8c7507312cc7f71811a3ba8138b2f2499fa3bb1d
Mat Waroff <matwaroff@gmail.com>
2015-12-16 22:54:04 -0500

    Fixing Times

--- pylight.py
+++ pylight.py
@@ -3,13 +3,17 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
-
-for i in range(0, 12):
-	GPIO.output(12, True)
-	print "ON"
-	time.sleep(.1)
+try:
+	for i in range(0, 100):
+		GPIO.output(12, True)
+		print "ON"
+		time.sleep(.2)
+		GPIO.output(12, False)
+		print "OFF"
+		time.sleep(.1)
+except KeyboardInterrupt:
+	print "OFF - EXITING"
 	GPIO.output(12, False)
-	print "OFF"
-	time.sleep(.2)
-
+	GPIO.cleanup()
 GPIO.cleanup()
+ 
