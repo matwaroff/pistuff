df79d30ca871490d73a5eb412dbbc7e5ea14e3d7
matwaroff <matwaroff@gmail.com>
2015-12-16 22:44:44 -0500

    Added blink to light

--- pylight.py
+++ pylight.py
@@ -4,10 +4,12 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 
-GPIO.output(12, True)
-print "ON"
-time.sleep(15)
-GPIO.output(12, True)
-print "OFF"
+for i in range(0, 12):
+	GPIO.output(12, True)
+	print "ON"
+	time.sleep(.1)
+	GPIO.output(12, False)
+	print "OFF"
+	time.sleep(.2)
 
 GPIO.cleanup()
