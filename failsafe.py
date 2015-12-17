2749d34b0e0e8a4cb155646f3238db7cc037960c
matwaroff <matwaroff@gmail.com>
2015-12-17 12:48:02 -0500

    Added Failsafe
    Will run every 5min

---
+++ failsafe.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+pinLED = 12
+pinServo = 11
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pinServo, GPIO.OUT) ## SERVO
+GPIO.setup(pinLED, GPIO.OUT) ## LED
+frequency = 50
+pwm = GPIO.PWM(pinServo, frequency)
+
+onPosition = .95
+offPosition = 1.5
+
+msPerCycle = 1000 / frequency
+count = 0
+GPIO.output(12, True) ##Lights On
+
+dutyCyclePercentage = offPosition * 100 / msPerCycle
+print "EMERGENCY Fail SAFE"
+pwm.start(dutyCyclePercentage)
+time.sleep(1)
+GPIO.output(12,False)
+pwm.stop()
+GPIO.cleanup()
